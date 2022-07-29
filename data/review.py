51253e7aa9b07ab119785be71e3e484eecc2fb4c
Vegard Broen <vegardkb@stud.ntnu.no>
2022-07-29 14:24:30 +0200

    Add requirements.txt and remove ICA from review.py

--- data/review.py
+++ data/review.py
@@ -1,8 +1,5 @@
 import matplotlib.pyplot as plt
 import numpy as np
-from sklearn.decomposition import FastICA
-from scipy import stats
-import time
 
 
 def load_trial(experiment, trial):
@@ -66,54 +63,8 @@ def plot_channels_fft(
     if show_legend:
         plt.legend()
 
-
-def ICA_filter(x, fs=500.0):
-    N_ch = x.shape[0]
-    ica = FastICA(n_components=N_ch)
-
-    S = ica.fit_transform(x.T)
-    Z = np.absolute(np.fft.fft(S, axis=0))
-    Z_freq = np.fft.fftfreq(S.shape[0], d=1 / fs)
-    Z_norm = (Z.T / np.exp(-0.0055 * Z_freq)).T
-
-    H = stats.entropy(Z_norm)
-    # H_t = (np.amin(H) + 2*np.mean(H)) / 3
-    H_t = np.median(H)
-    low_entropy = H < H_t
-
-    peak_mean_ratio = (np.amax(Z_norm, axis=0) - np.mean(Z_norm, axis=0)) / np.std(
-        Z_norm, axis=0
-    )
-    # pmr_t = (np.amax(peak_mean_ratio) + 2*np.mean(peak_mean_ratio)) / 3
-    pmr_t = np.median(peak_mean_ratio)
-    high_peak = peak_mean_ratio > pmr_t
-
-    bad_comp = np.logical_or(low_entropy, high_peak)
-    S_raw = S
-    S[:, bad_comp] = 0
-
-    y = ica.inverse_transform(S).T
-
-    return S_raw, S, y
-
-
 if __name__ == "__main__":
     ch = [1]
-    for i in range(21):
-        trialnum = i + 1
-
-        """ x = load_trial("Test", trialnum)
-        print(f"x shape: {x.shape}")
-        plot_channels(x, ch, voffset=1, title=f"Test Trial {trialnum}")
-        plot_channels_fft(x, ch, title=f"Test FFT Trial {trialnum}") """
-
-        """ x = load_trial("TestDeque", trialnum).T
-        print(f"x shape: {x.shape}")
-        plot_channels(x, ch, voffset=1, title=f"TestDeque Trial {trialnum}")
-        plot_channels_fft(x, ch, title=f"TestDeque FFT Trial {trialnum}") """
-
-        plt.show()
-
     for i in range(21):
         trialnum = i + 1
         exp_name = "Test2707"
@@ -121,13 +72,5 @@ if __name__ == "__main__":
         print(f"x shape: {x.shape}")
         plot_channels(x, ch, title=f"{exp_name} Trial {trialnum}")
         plot_channels_fft(x, ch, title=f"{exp_name} FFT Trial {trialnum}")
-
-        """ start_time = time.perf_counter()
-        S, S_filt, y = ICA_filter(x)
-        print(f"ICA_filter used {(time.perf_counter() - start_time)*1000} milliseconds")
-        plot_channels(S, [i+1 for i in range(x.shape[0])], title=f"{exp_name} Trial {trialnum} ICA components", voffset=1)
-        plot_channels(S_filt, [i+1 for i in range(x.shape[0])], title=f"{exp_name} Trial {trialnum} ICA components filtered", voffset=1)
-        plot_channels(y, ch, title=f"{exp_name} Trial {trialnum} ICA filtered")
-        plot_channels_fft(y, ch, title=f"{exp_name} FFT Trial {trialnum} ICA filtered") """
 
         plt.show()
